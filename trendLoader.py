442143ca0c5aa983fe2d074951e7893f8f517fe3
Ian Haehnlein <haehnlein.ian@gmail.com>
2025-07-18 12:49:15 -0500

    This is the first commit with fully operation app for plotting data exported from siemens trend page, including all files required for building the runfiles and installer.

---
+++ trendLoader.py
@@ -0,0 +1,94 @@
+from customtkinter import CTk, CTkButton, CTkRadioButton, CTkTextbox, CTkLabel, CTkFrame, CTkCheckBox
+from tkinter import LEFT, Text, BOTH, BooleanVar
+from utils import FileSelection, Plotter
+from os.path import basename, dirname
+from pandas import read_csv
+
+class App():
+    app = CTk()
+    fSelect = FileSelection()
+    runName = ''
+    unifiedHover:BooleanVar = BooleanVar(app,True,"unifiedHoverState")
+
+    frameSelectedFile = CTkFrame(app)
+    frameSelectedDir = CTkFrame(app)
+    tbSelectedFile = CTkTextbox(frameSelectedFile, width = 200, height = 25)
+    tbSelectedDir = CTkTextbox(frameSelectedDir, width = 200, height = 25)
+    lbSelectedFile = CTkLabel(frameSelectedFile,text='Selected File:', width = 70, height=25)
+    lbSelectedDir = CTkLabel(frameSelectedDir,text='Selected Directory:', width = 70, height=25)
+    frameSave = CTkFrame(app)
+
+    def __init__(self) -> None:
+        try:
+            self.app.iconbitmap('GP_Plasma_StackedOrg.ico')
+        except Exception as e:
+            print(e)
+        self.app.title('Siemens Trend Export Plotter')
+        self.app.resizable(True,True)
+        self.app.geometry('400x400')
+
+        self.open_button = CTkButton(
+            self.app,
+            text = 'Select Trend File',
+            command = self.open_file
+        )
+        self.btnPlotFile = CTkButton(self.app, text='PlotData', state='disabled', command = self.plot_selected_file)
+        self.btnSaveInteractive = CTkButton(self.frameSave, text = 'Save Interactive', state = 'disabled', command = self.saveInteractive)
+        self.btnSavePng = CTkButton(self.frameSave, text = 'Save PNG', state = 'disabled', command = self.savePng)
+        self.cbSelectHoverMode = CTkCheckBox(self.app, text = "Unified Marker Display", variable=self.unifiedHover)
+
+        padX = 5
+        padY = 5
+        anchor = 'center'
+        self.open_button.pack(anchor=anchor, expand=True, fill=BOTH, padx=padX*2, pady=padY)
+        self.frameSelectedDir.pack(anchor=anchor, expand=True, fill=BOTH, padx=padX, pady=padY)
+        self.frameSelectedFile.pack(anchor=anchor, expand=True, fill=BOTH, padx=padX, pady=padY)
+        self.lbSelectedFile.pack(padx=padX, pady=padY, anchor=anchor, expand=False, fill=BOTH, side=LEFT)
+        self.lbSelectedDir.pack(padx=padX, pady=padY, anchor=anchor, expand=False, fill=BOTH, side=LEFT)
+        self.tbSelectedFile.pack(padx=padX, pady=padY, anchor=anchor, expand=True, fill=BOTH, side=LEFT)
+        self.tbSelectedDir.pack(padx=padX, pady=padY, anchor=anchor, expand=True, fill=BOTH, side=LEFT)
+
+        self.cbSelectHoverMode.pack(anchor=anchor)
+        self.btnPlotFile.pack(anchor=anchor, expand=True, fill=BOTH, padx=padX*2, pady=padY)
+        self.frameSave.pack(anchor=anchor, expand=True, fill=BOTH, padx=padX, pady=padY)
+        self.btnSaveInteractive.pack(anchor=anchor, padx=padX, pady=padY, expand=True, fill=BOTH, side=LEFT)
+        self.btnSavePng.pack(anchor=anchor, padx=padX, pady=padY, expand=True, fill=BOTH, side=LEFT)
+
+    
+    def open_file(self):
+        fName = self.fSelect.select_file()
+        self.runName = basename(self.fSelect.filename.rstrip('.csv'))
+        self.btnPlotFile.configure(state='normal')
+        self.tbSelectedFile.configure(require_redraw=False,state='normal')
+        self.tbSelectedFile.delete('0.0','end')
+        self.tbSelectedFile.insert('0.0',basename(fName))
+        self.tbSelectedFile.configure(require_redraw=False,state='disabled')
+        self.tbSelectedDir.configure(require_redraw=False,state='normal')
+        self.tbSelectedDir.delete('0.0','end')
+        self.tbSelectedDir.insert('0.0',dirname(fName))
+        self.tbSelectedDir.configure(require_redraw=False,state='disabled')
+        pass
+
+    def plot_selected_file(self):
+        self.plotter = Plotter(basename(self.fSelect.filename.rstrip('.csv')),self.unifiedHover.get())
+        sysData = read_csv(self.fSelect.filename, delimiter=';')
+        self.plotter.plotData(sysData)
+        self.plotter.fig.show()
+        self.btnSaveInteractive.configure(state='noraml')
+        self.btnSavePng.configure(state='noraml')
+        pass
+
+    def saveInteractive(self):
+        saveFile = FileSelection()
+        saveFile.select_save(fType='.html',defaultFile=self.runName)
+        self.plotter.fig.write_html(file=saveFile.filename, include_plotlyjs=True)
+        pass
+
+    def savePng(self):
+        saveFile = FileSelection()
+        saveFile.select_save(fType='.png',defaultFile=self.runName)
+        self.plotter.fig.write_image(file=saveFile.filename, format='png',width=1600, height=900)
+        pass
+
+plotter = App()
+plotter.app.mainloop()
---
+++ utils.py
@@ -0,0 +1,169 @@
+from pprint import pprint
+from customtkinter import CTkTextbox
+from tkinter import filedialog
+from plotly.graph_objects import Figure, Scatter, Table
+from plotly.subplots import make_subplots
+import plotly.express as px
+import plotly.io as pio
+from pandas import DataFrame
+
+class FileSelection():
+    filename:str = ''
+
+    def select_file(self,fType:str='.csv',defaultFile=''):
+        match fType:
+            case '.csv': filetypes = [('Comma Separated Values', '*.csv')]
+            case '.html': filetypes = [('Interactive Plot', '*.html')]
+            case '.png': filetypes = [('Portable Network Graphic', '*.png')]
+
+        filename = filedialog.askopenfilename(
+            title = 'Select trend export file',
+            initialdir = 'C:/',
+            filetypes=filetypes,
+            initialfile=defaultFile  
+        )
+        self.filename = filename
+        return filename
+    
+    def select_save(self,fType:str='.html',defaultFile=''):
+        match fType:
+            case '.csv': filetypes = [('Comma Separated Values', '*.csv')]
+            case '.html': filetypes = [('Interactive Plot', '*.html')]
+            case '.png': filetypes = [('Portable Network Graphic', '*.png')]
+        filename = filedialog.asksaveasfilename(defaultextension=fType, filetypes=filetypes, initialfile=defaultFile)
+        self.filename = filename
+        return filename
+
+        
+    
+class Plotter():
+    fig:Figure
+    colorMap:list[str] = px.colors.qualitative.Light24
+    pressuresList: list[str] = []
+    processTrends: list[str] = []
+    config:dict = {}
+
+    def __init__(self,runName:str, withHoverUnified:bool = True) -> None:
+        h_space = 0.05
+        v_space = 0.05
+        widths = []
+        self.fig = make_subplots(
+            rows=2,
+            cols=1,
+            vertical_spacing=v_space,
+            horizontal_spacing=h_space,
+            specs= [
+                [{"secondary_y":False}],
+                [{"secondary_y":False}]
+            ],
+            subplot_titles=(
+                'System Pressures',
+                'Process Trends'
+            ),
+            shared_xaxes=True
+        )
+        self.fig.update_yaxes(row=1,col=1, type='log', tickformat='0.0e', title_text='Pressure [mTorr]', minor=dict(showgrid=True), tickmode='array', tickvals=[1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,1,1e1,1e2])
+        self.fig.update_xaxes(row=1,col=1, nticks = 20)
+        self.fig.update_yaxes(row=2,col=1, type='linear', title_text='Trends')
+        self.fig.update_xaxes(
+            row=2,
+            col=1,
+            title_text='Time Stamp',
+            tickangle = 360-45,
+            nticks = 20
+        )
+        if withHoverUnified:
+            hovermode = "x unified"
+        else:
+            hovermode = "closest"
+        self.fig.update_layout(
+            title = dict(text=runName),
+            margin = dict(l=10,r=10,t=50,b=10),
+            template = 'plotly_dark',
+            hovermode = hovermode
+        )
+
+    def addPlotTrace(self, data:DataFrame, dataKey:str, lineColor:str, lineDash:str='solid', row:int=1, col:int=1, secondary_y:bool=False,log_y:bool=False):
+        if secondary_y:
+            label = dataKey + ' (right)'
+        else:
+            label = dataKey
+
+        if log_y:
+            htemp = "%{y:0.3e}"
+        else:
+            htemp = "%{y:0.2f}"
+        self.fig.add_trace(
+            Scatter(
+                x = data[self.config[dataKey]['x']],
+                y = data[self.config[dataKey]['y']],
+                line_color = lineColor,
+                line_dash = lineDash,
+                name = f'{label}',
+                hovertemplate=htemp
+            ),
+            row = row,
+            col = col,
+            secondary_y = secondary_y
+        )
+        pass
+
+    def plotData(self, data:DataFrame):
+        dataKeys = self.sortKeys(data.columns.to_list())
+        colorIteration = 0
+        lineIteration = 0
+        for tag in self.config:
+            if tag in self.pressuresList:
+                rowSet = 1
+                colSet = 1
+                log_y = True
+            elif tag in self.processTrends:
+                rowSet = 2
+                colSet = 1
+                log_y = False
+
+            color = self.colorMap[colorIteration%len(self.colorMap)]
+            if colorIteration%len(self.colorMap) == len(self.colorMap):
+                lineIteration += 1
+            
+            match lineIteration:
+                case 0: dash = 'solid'
+                case 1: dash = 'dash'
+                case 2: dash = 'dashdot'
+                case 3: dash = 'longdash'
+                case 4: dash = 'longdashdot'
+                case _: dash = 'dot'
+
+            self.addPlotTrace(
+                data = data,
+                dataKey = tag,
+                lineColor = color,
+                lineDash = dash,
+                row = rowSet,
+                col = colSet,
+                log_y=log_y
+            )
+            colorIteration += 1
+
+    def sortKeys(self, columns:list[str]):
+        allkeys = []
+        pprint(columns)
+        for key in columns:
+            if 'Time' not in key:
+                tag = key.replace('Y value','').rstrip()
+                if 'Gauge' in key:
+                    self.pressuresList.append(tag)
+                else:
+                    self.processTrends.append(tag)
+                allkeys.append(tag)
+                self.config.update(
+                    {
+                        tag:{
+                            'x': tag + ' Time',
+                            'y': key
+                        }
+                    }
+                )
+        print(allkeys)
+        pprint(self.config)
+        return allkeys
